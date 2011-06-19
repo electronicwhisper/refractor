bb0a67d172f213e9ce7f2bc7a7024b566f7df70d
Scott Ostler <scott@lascaux.us>
2011-06-19 13:17:16 -0400

    Really listen on env.PORT

--- app.js
+++ app.js
@@ -35,5 +35,5 @@ app.get('/', function(req, res){
 });
 
 var port = process.env.PORT || 3000;
-app.listen(3000);
+app.listen(port);
 console.log("Express server listening on port %d", app.address().port);
